7a33b4367c94dad120943e43ad5b89f4ee3bbfc5
Anton Rutkevich <anton@gojuno.com>
2019-06-01 20:45:30 +0300

    Add output package flag

--- cmd/genkotlin/main.go
+++ cmd/genkotlin/main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/mkorolyov/astparser"
 	"github.com/mkorolyov/genkotlin/kotlin"
@@ -15,8 +16,9 @@ var (
 	inputDir         = flag.String("in", "", "directory with go files to be parsed")
 	excludeRegexpStr = flag.String("e", "", "exclude regexp to skip files")
 	includeRegexpStr = flag.String("i", "", "include regexp to limit input files")
-	outputDir        = flag.String("o", "", "output directory for generated files")
-	useJsonNames     = flag.Bool("json_names", false, "use json tag for field names")
+	outputDir        = flag.String("o", "", "output directory for generated files, without package")
+	outPackage       = flag.String("package", "", "package for generated kt files")
+	useJsonNames     = flag.Bool("json_names", false, "use json tag for field names, eg com.myapp")
 )
 
 func main() {
@@ -35,15 +37,32 @@ func main() {
 		log.Fatalf("failed to load sources from %s excluding %s: %v", *inputDir, *excludeRegexpStr, err)
 	}
 
-	generator := kotlin.NewGenerator(kotlin.Config{UseJsonTagNames: *useJsonNames})
+	config := kotlin.Config{UseJsonTagNames: *useJsonNames}
+	if *outPackage != "" {
+		config.OutPackage = *outPackage
+	}
+
+	generator := kotlin.NewGenerator(config)
 	// generate kotlin classes
 	if *outputDir != "" {
 		kotlinFiles := generator.Generate(sources)
 		// save
 		for f, body := range kotlinFiles {
-			filePath := *outputDir + "/" + f + ".kt"
-			if err := ioutil.WriteFile(filePath, body, 0666); err != nil {
-				fmt.Fprintf(os.Stderr, "failed to save generated kotlin file %s: %v", filePath, err)
+			outPackagePath := ""
+			if *outPackage != "" {
+				outPackagePath = strings.Join(strings.Split(*outPackage, "."), "/")
+			}
+
+			// 0666 is enough, but https://github.com/golang/go/issues/22323
+			packagePath := *outputDir + "/" + outPackagePath
+			if err := os.MkdirAll(packagePath, 0777); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to create directory %s: %v\n", packagePath, err)
+			}
+
+			filePath := packagePath + "/" + f + ".kt"
+
+			if err := ioutil.WriteFile(filePath, body, 0777); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to save generated kotlin file %s: %v\n", filePath, err)
 			}
 		}
 	}
